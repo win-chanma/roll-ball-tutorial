4c096c9777adedd5a2508abfc1071e32192919a1
win-chanma <t.ueki.dev@gmail.com>
2019-11-24 11:13:25 +0900

    Remove some space

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Text countText;
     public Text winText;
-    
+
     private Rigidbody rb;
     private int count;
 
@@ -20,17 +20,17 @@ public class PlayerController : MonoBehaviour
         SetCountText();
         winText.text = "";
     }
-    
+
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        
-        Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
     }
 
-    //　Collider利用した衝突判定
+    // Collider利用した衝突判定
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
@@ -50,4 +50,3 @@ public class PlayerController : MonoBehaviour
         }
     }
 }
-
