9eb6646bc12b4ef4e6bd5c489d725950ec19f06b
win-chanma <t.ueki.dev@gmail.com>
2019-11-24 11:06:03 +0900

    3-3.スコアとテキストの表示

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public Rigidbody rb;
+    public Text countText;
+    public Text winText;
+    
+    private Rigidbody rb;
+    private int count;
+
     // See also:https://docs.unity3d.com/ja/2019.1/Manual/ExecutionOrder.html
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     
     private void FixedUpdate()
@@ -26,7 +35,18 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
-            other.gameObject.SetActive(false);            
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count :" + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
